14f63f1658c6f0b26d0481a48e1fd8b07fc4e8b6
Kristof Lettens <kristof.lettens@sdworx.com>
2023-09-06 13:41:08 +0200

    Add test for maximum quality of 50 for aged brie and backstagepasses

--- csharpcore/GildedRoseTests/GildedRoseTest.cs
+++ csharpcore/GildedRoseTests/GildedRoseTest.cs
@@ -7,11 +7,25 @@ namespace GildedRoseTests;
 public class GildedRoseTest
 {
     [Fact]
-    public void Foo()
+    public void ItemQuality_ItemIsAgedBrie_ShouldNeverBeMoreThanFifty()
     {
-        IList<Item> items = new List<Item> { new Item { Name = "foo", SellIn = 0, Quality = 0 } };
+        IList<Item> items = new List<Item> { new Item { Name = "Aged Brie", SellIn = 0, Quality = 50 } };
         GildedRose app = new GildedRose(items);
+
         app.UpdateQuality();
-        Assert.Equal("fixme", items[0].Name);
+
+        Assert.True(50 >= items[0].Quality);
+    }
+
+    [Fact]
+    public void ItemQuality_BackstagePasses_ShouldNeverBeMoreThanFifty()
+    {
+        IList<Item> items = new List<Item>
+            { new Item { Name = "Backstage passes to a TAFKAL80ETC concert", SellIn = 1, Quality = 50 } };
+        GildedRose app = new GildedRose(items);
+
+        app.UpdateQuality();
+
+        Assert.True(50 >= items[0].Quality);
     }
 }
